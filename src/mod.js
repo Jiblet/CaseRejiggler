010e06b4a2183bc9779be6bb09fa4593e8d9e6f5
Simon Peel <simon.peel@gmail.com>
2022-02-14 10:00:51 +0000

    Improved logging and made disable actually disable

--- src/mod.js
+++ src/mod.js
@@ -22,13 +22,16 @@ class Mod {
     //Check we're enabled in config and rejig the cases using the values in config.json
     if (config.Settings.Enabled) {
       if (config.Settings.Logging) {
-        Logger.log(`{[${modName} : ${version}]} : ----- Enabled - Begin the rejiggling -----`, "white", "green")
+        Logger.log(`{[${modName} : ${version}]} : ----- Enabled - Begin the rejigging -----`, "white", "green")
       }
 
       for (let item in JSONItems) {
         let currentItem = JSONItems[item] //setup a currentItem object to pull params from
         if (items[currentItem.id] == undefined) {
           Logger.log(`{[${modName} : ${version}]} : ERROR! : Item '${item}' with ID ${currentItem.id} is not found in SPT database - please check ID on https://db.sp-tarkov.com/`, "white", "red");
+          if (config.Settings.Logging) {
+            Logger.log(`{[${modName} : ${version}]} : -----`); //I know, I know.
+          }
           continue;
         }
 
@@ -43,24 +46,30 @@ class Mod {
         } else {
           Logger.log(`{[${modName} : ${version}]} : WARNING : '${item}', ID: ${currentItem.id} is not enabled for rejigging; skipped.`, "red"); //Warn without checking logging
         }
+        if (config.Settings.Logging) {
+          Logger.log(`{[${modName} : ${version}]} : Rejigging of '${item}', ID: ${currentItem.id} completed`, "white", "green");
+          Logger.log(`{[${modName} : ${version}]} : -----`);
+        }
       }
+    } else { //config.json has Settings.Enabled flag set to false so we're out of here.
+      Logger.log(`[${modName} : ${version}] : ----- Disabled in config.json - Skipping all rejigging -----`, "red")
+      return;
     }
 
     //Work Complete.
     if (config.Settings.Logging) {
-      Logger.log(`{[${modName} : ${version}]} : ----- Rejiggling complete -----`, "white", "green")
+      Logger.log(`{[${modName} : ${version}]} : ----- Rejigging complete -----`, "white", "green")
     }
   }
 
   /** ============================= */
   /** ========= Functions ========= */
 
-  setItemInternalSize(id, newHorizontal, newVertical) { //TODO Verify if integers in reasonable range (1 - 15? 20?), vomit exceptions
+  setItemInternalSize(id, newHorizontal, newVertical) { //TODO Verify if integers in reasonable range (1 - 15? 20?), vomit exceptions //IF were making it smaller we should shout
     items[id]._props.Grids[0]._props.cellsH = newHorizontal;
     items[id]._props.Grids[0]._props.cellsV = newVertical;
     if (config.Settings.Logging) {
       Logger.log(`{[${modName} : ${version}]} : Resized ${id} to: ${items[id]._props.Grids[0]._props.cellsH} x ${items[id]._props.Grids[0]._props.cellsV}`, "white", "blue");
-      Logger.log(`{[${modName} : ${version}]} : -----`);
     }
   }
 
@@ -86,14 +95,14 @@ class Mod {
     if (config.Settings.Logging) {
       Logger.log(`{[${modName} : ${version}]} : ${id} NEW flea price : ${newFleaPrice}`, "white", "cyan")
       Logger.log(`{[${modName} : ${version}]} : ${id} NEW handbook price : ${newHbPrice}`, "white", "cyan")
-      Logger.log(`{[${modName} : ${version}]} : -----`)
     }
   }
 
-  addToItemFilter(id, additionalItems) { //TODO Are the IDs in additionalItems actually valid? vomit exceptions
-    if (additionalItems === "") {
+  addToItemFilter(id, additionalItems) {
+    if (additionalItems === "") { //are there any additional items?
       if (config.Settings.Logging) {
-        Logger.log("No extra filter items", "white", "magenta");
+        Logger.log(`{[${modName} : ${version}]} : No additional filter items`, "white", "magenta");
+        return; //nothing to do , so back we go.
       }
     } else {
       if (config.Settings.Logging) {
@@ -102,9 +111,9 @@ class Mod {
       }
       //validate the items in the additional items list against SPT items DB
       for (const itemKey in additionalItems) {
-        if (items[additionalItems[itemKey]] == undefined){
+        if (items[additionalItems[itemKey]] == undefined) {
           Logger.log(`{[${modName} : ${version}]} : ERROR! : Additional Filter Object with ID '${additionalItems[itemKey]}' is not found in SPT database; skipped - please check ID on https://db.sp-tarkov.com/`, "white", "red");
-          continue;//log the error then skip this undefined item
+          continue; //log the error then skip this undefined item
         }
         items[id]
           ._props
@@ -116,7 +125,6 @@ class Mod {
       }
       if (config.Settings.Logging) {
         Logger.log(`{[${modName} : ${version}]} : Now ${items[id]._props.Grids[0]._props.filters[0].Filter}`, "yellow", "magenta")
-        Logger.log(`{[${modName} : ${version}]} : -----`)
       }
     }
   }
